5d501aac4ebb3f83c98210e7dab72e9737fb572c
cherwellwine <cherwellwine.sg@gmail.com>
2019-02-16 23:27:07 +0800

    test terraform directory structure

---
+++ terraform/variables.tf
@@ -0,0 +1,101 @@
+# region = "us-west-2"
+# public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCRQYYw42SeWm4SvTt4iipyjozdpaR5FlNeyo3oWYXF2W2Uur+XXJssFhxD8xf0NFKRtrK3wVZPkP/7k6+eRufC9Lq6VZvImlCTFJmEy+uHnA+vlkoXbUGk2zr7Cpct7udpZZxSivt+7lQ4avhBCQE/hw1qZxdGyZJY1Z1F3LGHCP55a+h5XxtaZR0eJQmejWnG9wq++iywdeOH2tCeOsnyNw1bjhYfydEDK7OAh/sZYsroxGpk/0SNFyscy/x2zEBrveDppE6QlH9pffx50mV00OgefHx2wpa95jwG7RWKvUkwqqMx1bEbg7tZ76PfNTM/rRg+EV9d1NMKQ3R5S1zT"
+# name = "terraform-aws-three-tier"
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "name" {
+  default = "terraform-aws-three-tier"
+}
+
+variable "public_key"{
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCRQYYw42SeWm4SvTt4iipyjozdpaR5FlNeyo3oWYXF2W2Uur+XXJssFhxD8xf0NFKRtrK3wVZPkP/7k6+eRufC9Lq6VZvImlCTFJmEy+uHnA+vlkoXbUGk2zr7Cpct7udpZZxSivt+7lQ4avhBCQE/hw1qZxdGyZJY1Z1F3LGHCP55a+h5XxtaZR0eJQmejWnG9wq++iywdeOH2tCeOsnyNw1bjhYfydEDK7OAh/sZYsroxGpk/0SNFyscy/x2zEBrveDppE6QlH9pffx50mV00OgefHx2wpa95jwG7RWKvUkwqqMx1bEbg7tZ76PfNTM/rRg+EV9d1NMKQ3R5S1zT"
+}
+
+# VPC Variables
+variable "vpc_env" {
+  description = "VPC environment"
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  default     = "10.7.0.0/16"
+}
+
+variable "vpc_azs" {
+  description = "A list of availability zones in the region"
+  default     = [ "us-west-2a", "us-west-2b", "us-west-2c"]
+}
+
+variable "vpc_public_subnets" {
+  description = "A list of public subnets inside the VPC"
+  default     = ["10.7.1.0/24", "10.7.2.0/24", "10.7.3.0/24"]
+}
+
+variable "vpc_private_subnets" {
+  description = "A list of private subnets inside the VPC"
+  default     = ["10.7.11.0/24", "10.7.12.0/24", "10.7.13.0/24"]
+}
+
+variable "vpc_database_subnets" {
+  type        = "list"
+  description = "A list of database subnets"
+  default     = ["10.7.21.0/24", "10.7.22.0/24", "10.7.23.0/24"]
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+  default     = true
+}
+
+variable "vpc_single_nat_gateway" {
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+  default     = false
+}
+
+variable "vpc_one_nat_gateway_per_az" {
+  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
+  default     = true
+}
+
+# vpc_azs = [ "us-west-2a", "us-west-2b", "us-west-2c"]
+# vpc_cidr = "10.7.0.0/16"
+# vpc_public_subnets = ["10.7.1.0/24", "10.7.2.0/24", "10.7.3.0/24"]
+# vpc_pirvate_subnets  = ["10.7.11.0/24", "10.7.12.0/24", "10.7.13.0/24"]
+# vpc_database_subnets = ["10.7.21.0/24", "10.7.22.0/24", "10.7.23.0/24"]
+# vpc_enable_nat_gateway = true
+# vpc_single_nat_gateway = false
+# vpc_one_nat_gateway_per_az = true
+# vpc_env = "dev"
+
+# DB
+# db_identifier = "terraformaws"
+# db_name = "taws"
+# db_username = "tawsdbuser"
+# db_password = "tawsdbpassword"
+
+#db_allocated_storage =
+#db_port =
+#db_backup_window =
+#db_backup_retention_period =
+#db_maintenance_window =
+
+#app_port =
+#app_instance_type =
+#app_autoscale_min_size =
+#app_autoscale_max_size =
+#app_elb_health_check_interval =
+#app_elb_healthy_threshold =
+#app_elb_unhealthy_threshold =
+#app_elb_health_check_timeout =
+
+#web_port =
+#web_instance_type =
+#web_autoscale_min_size =
+#web_autoscale_max_size =
+#web_elb_health_check_interval =
+#web_elb_healthy_threshold =
+#web_elb_unhealthy_threshold =
+#web_elb_health_check_timeout =
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
@@ -15,47 +19,48 @@ module "vpc" {
 
   tags {
     Group = "${var.name}"
-  } 
+    Environment = "${var.vpc_env}"
+  }
 }
 
 # Borrowed from VPC Module from Terraform Module Repository:
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
-  default     = "0.0.0.0/0"
-}
-
-variable "vpc_public_subnets" {
-  description = "A list of public subnets inside the VPC"
-  default     = []
-}
-
-variable "vpc_private_subnets" {
-  description = "A list of private subnets inside the VPC"
-  default     = []
-}
-
-variable "vpc_database_subnets" {
-  type        = "list"
-  description = "A list of database subnets"
-  default     = []
-}
-
-variable "vpc_azs" {
-  description = "A list of availability zones in the region"
-  default     = []
-}
-
-variable "vpc_enable_nat_gateway" {
-  description = "Should be true if you want to provision NAT Gateways for each of your private networks"
-  default     = false
-}
-
-variable "vpc_single_nat_gateway" {
-  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
-  default     = false
-}
-
-variable "vpc_one_nat_gateway_per_az" {
-  description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
-  default     = false
-}
+# variable "vpc_cidr" {
+#   description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+#   default     = "0.0.0.0/0"
+# }
+#
+# variable "vpc_public_subnets" {
+#   description = "A list of public subnets inside the VPC"
+#   default     = []
+# }
+#
+# variable "vpc_private_subnets" {
+#   description = "A list of private subnets inside the VPC"
+#   default     = []
+# }
+#
+# variable "vpc_database_subnets" {
+#   type        = "list"
+#   description = "A list of database subnets"
+#   default     = []
+# }
+#
+# variable "vpc_azs" {
+#   description = "A list of availability zones in the region"
+#   default     = []
+# }
+#
+# variable "vpc_enable_nat_gateway" {
+#   description = "Should be true if you want to provision NAT Gateways for each of your private networks"
+#   default     = false
+# }
+#
+# variable "vpc_single_nat_gateway" {
+#   description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks"
+#   default     = false
+# }
+#
+# variable "vpc_one_nat_gateway_per_az" {
+#   description = "Should be true if you want only one NAT Gateway per availability zone. Requires `var.azs` to be set, and the number of `public_subnets` created to be greater than or equal to the number of availability zones specified in `var.azs`."
+#   default     = false
+# }
